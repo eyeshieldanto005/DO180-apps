19e2acfe4c9c4ab218facbc0bb1610046d06b72d
aprianto <73568213+eyeshieldanto005@users.noreply.github.com>
2022-04-05 14:42:04 +0700

    Update server.js

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -4,10 +4,33 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World from pod: ' + process.environment.HOSTNAME + '\n')
+  res.send('Booking from pod: ' + process.environment.HOSTNAME + '\n')
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('App listening on port 8080!');
 });
+const d = new Date();
+d.setDate(30);
+
+const d = new Date();
+d.setHours(24);
+
+const d = new Date();
+d.setSeconds(60);
+
+let text = "Pilih jadwal pakarmu";
+const today = new Date();
+const someday = new Date();
+someday.setFullYear(2100, 0, 14);
+
+if (someday > today) {
+  text = "hari ini";
+} else {
+  text = "bukan hari ini";
+}
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const d = new Date();
+let month = months[d.getMonth()];
 
